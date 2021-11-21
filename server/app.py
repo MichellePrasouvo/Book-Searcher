bfe380e7b5ffe984ad3e2e671cf1844bd6c449e8
Henry Reyes <reyesh2@uci.edu>
2021-11-20 18:56:44 -0800

    changed algo

--- server/app.py
+++ server/app.py
@@ -15,21 +15,25 @@ def home():
     resp = urlopen(api + name.replace(" ", ""))
     book_data = json.load(resp)
 
-    book_info = book_data["items"][0]["volumeInfo"]
+    book_infos = []
+    for i in book_data["items"]:
+        book_info = i["volumeInfo"]
 
-    book_dict = {}
-    key_list = ['authors', 'pageCount', "publishedDate", 'averageRating', 'title', 'description', "imageLinks"]
-    for key in key_list:
-        if key in book_info:
-            book_dict[key] = book_info[key]
-        else:
-            book_dict[key] = ''
+        book_dict = {}
+        key_list = ['authors', 'pageCount', "publishedDate", 'averageRating', 'title', 'description', "imageLinks"]
+        for key in key_list:
+            if key in book_info:
+                book_dict[key] = book_info[key]
+            else:
+                book_dict[key] = ''
 
-    if 'authors' in book_dict:
-        book_dict['authors'] = book_dict['authors'][0]
-    if 'imageLinks' in book_dict:
-        book_dict['imageLinks'] = book_dict['imageLinks']['thumbnail']
-    return book_dict
+        if 'authors' in book_dict:
+            book_dict['authors'] = book_dict['authors'][0]
+        if 'imageLinks' in book_dict:
+            book_dict['imageLinks'] = book_dict['imageLinks']['thumbnail']
+        book_infos.append(book_dict)
+    result = {'results': book_infos}
+    return result
             
 
 
